eb15a8699e0a58ce91aac6124a795ddf8792c689
Prasad Ravan <prasadcravan@gmail.com>
2022-08-01 22:44:37 +0530

    Day1

---
+++ LeetCode/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeDay1Program1
+{
+    public static class RunningSumCalculator
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 1, 1 };
+
+            RunningSum(nums);
+
+            foreach (var element in nums)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ LeetCodeDay1Program2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeDay1Program2
+{
+    public static class PivotFinder
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, -1 };
+
+            Console.WriteLine(PivotIndex(nums));
+
+            Console.ReadLine();
+        }
+
+        public static int PivotIndex(int[] nums)
+        {
+            int totalSum = 0, leftSum = 0;
+
+            foreach (var element in nums)
+            {
+                totalSum += element;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (totalSum - nums[i] == leftSum)
+                {
+                    return i;
+                }
+                else
+                {
+                    totalSum -= nums[i];
+                    leftSum += nums[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
