293c94013157b53ecf06761351adc6e550428ab4
Prasad Ravan <prasadcravan@gmail.com>
2022-08-02 23:29:09 +0530

    Day2

---
+++ LeetCodeDay2Program1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeDay2Program1
+{
+    internal class IsomorphicString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsIsomorphicString("bbbc", "babd"));
+            Console.ReadLine();
+        }
+
+        public static bool IsIsomorphicString(string s, string t)
+        {
+            Dictionary<char, char> characterMaps = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var keyValuePair = characterMaps.Where(k => k.Value == t[i]).FirstOrDefault();
+
+                if (keyValuePair.Key != Char.MinValue && keyValuePair.Key != s[i])
+                {
+                    return false;
+                }
+                else if (keyValuePair.Key == Char.MinValue)
+                {
+                    var isSuccess = characterMaps.TryAdd(s[i], t[i]);
+
+                    if (!isSuccess)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeDay2Program2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeDay2Program2
+{
+    internal class Subsequence
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsSubsequence("aaaaaaa","bbaaaa"));
+            Console.ReadLine();
+        }
+
+        public static bool IsSubsequence(string s, string t)
+        {
+            int sl = 0;
+
+            foreach (char c in t)
+            {
+                if (sl < s.Length && s[sl] == c)
+                {
+                    sl++;
+                }
+            }
+
+            return sl == s.Length;
+        }
+    }
+}
