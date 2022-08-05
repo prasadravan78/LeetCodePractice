73269d4191d07be42862d04298c6c3335e020649
Prasad Ravan <prasadcravan@gmail.com>
2022-08-05 22:53:36 +0530

    Day5

---
+++ LeetCodeDay5Program1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeDay5Program1
+{
+    internal class BuySellStock
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxProfit(new int[] { 3, 2, 6, 5, 0, 3 }));
+            Console.ReadLine();
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int max = 0;
+            int min = prices[0];
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (min < prices[i])
+                {
+                    max = Math.Max(prices[i] - min, max);
+                }
+                else
+                {
+                    min = prices[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCodeDay5Program2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeDay5Program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestPalindrome("accca"));
+            Console.ReadLine();
+        }
+
+        public static int LongestPalindrome(string s)
+        {
+            HashSet<char> chars = new HashSet<char>();
+            var longestPalindrome = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (chars.Contains(s[i]))
+                {
+                    longestPalindrome += 2;
+                    chars.Remove(s[i]);
+                }
+                else
+                {
+                    chars.Add(s[i]);
+                }
+            }
+
+            if (chars.Count() > 0)
+            {
+                longestPalindrome++;
+            }
+
+            return longestPalindrome;
+        }
+    }
+}
