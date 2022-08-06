07c3e634d9866c9aebda69fbf726456b9793b813
Prasad Ravan <prasadcravan@gmail.com>
2022-08-06 23:24:07 +0530

    Day6

---
+++ LeetCodeDay6Program1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCodeDay6Program1
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    internal class NaryPreorderTreeTraversal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public IList<int> Preorder(Node root)
+        {
+            var preOrderNodes = new List<int>();
+
+            if (root != null)
+            {
+                preOrderNodes.Add(root.val);
+
+                if (root.children != null)
+                {
+                    foreach (var node in root.children)
+                    {
+                        preOrderNodes.AddRange(Preorder(node));
+                    }
+                }
+            }
+
+            return preOrderNodes;
+        }
+    }
+}
---
+++ LeetCodeDay6Program2/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace LeetCodeDay6Program2
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Recursive Solution
+    internal class BinaryTreeLevelOrderTraversal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            IList<IList<int>> levelOrder = new List<IList<int>>();
+
+            if (root != null)
+            {
+                BinaryLevelOrder(root, 0, ref levelOrder);
+
+            }
+
+            return levelOrder;
+        }
+
+        private void BinaryLevelOrder(TreeNode root, int level, ref IList<IList<int>> levelOrder)
+        {
+            if (root != null)
+            {
+                if (levelOrder.Count == level)
+                {
+                    levelOrder.Add(new List<int>());
+                }
+
+                levelOrder[level].Add(root.val);
+                BinaryLevelOrder(root.left, level + 1, ref levelOrder);
+                BinaryLevelOrder(root.right, level + 1, ref levelOrder);
+            }
+
+            return;
+        }
+
+        // Queue Solution
+        public IList<IList<int>> LevelOrderIter(TreeNode root)
+        {    
+            IList<IList<int>> levelOrder = new List<IList<int>>();       
+            Queue<TreeNode> treeNodes = new Queue<TreeNode>();              
+
+            if (root != null)
+            {
+                levelOrder.Add(new List<int>() { root.val });
+                treeNodes.Enqueue(root);
+            }
+
+            while (treeNodes.Any())
+            {
+                List<int> lvl = new List<int>();
+                int qSize = treeNodes.Count;
+
+                for (int i = 0; i < qSize; ++i)
+                { 
+                    TreeNode node = treeNodes.Dequeue();
+
+                    if (node.left != null)
+                    {
+                        lvl.Add(node.left.val);
+                        treeNodes.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        lvl.Add(node.right.val);
+                        treeNodes.Enqueue(node.right);
+                    }
+                }
+
+                if (lvl.Any())
+                {
+                    levelOrder.Add(lvl); 
+                }
+            }
+
+            return levelOrder;
+        }
+    }
+}
