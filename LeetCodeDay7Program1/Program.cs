dd9587f1257fed7f1858923ae0d3826e13aa138d
Prasad Ravan <prasadcravan@gmail.com>
2022-08-07 21:28:01 +0530

    Day7

---
+++ LeetCodeDay7Program1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeDay7Program1
+{
+    internal class BinarySearch
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Search(new int[] { -1, 0, 3, 5, 9, 12 }, 0));
+            Console.ReadKey();
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+            int min = 0;
+            int max = nums.Length - 1;
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+
+                if (target == nums[mid])
+                {
+                    return mid;
+                }
+                else if (target < nums[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
